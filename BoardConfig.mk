cf043a4742096b5f1785aa7f0574698ec1e210cf
chandu078 <chandudyavanapelli03@gmail.com>
2026-09-16 08:59:22 +0000

    infiniti: Switch to source built kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,13 +20,6 @@ TARGET_OTA_ASSERT_DEVICE := OP60FFL1,OP611FL1
 # Display
 TARGET_SCREEN_DENSITY := 540
 
-# Kernel
-ifeq ($(USE_PREBUILT_KERNEL), true)
-include device/oneplus/infiniti-kernel/BoardConfig.mk
-else
-TARGET_KERNEL_ADDITIONAL_FLAGS += CONFIG_INFINITI_DTB=y
-endif
-
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
